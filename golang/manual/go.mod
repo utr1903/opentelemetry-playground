ccebc16da7a15b9fa3939b48b3fa120d02a7efef
uturkarslan <ugurturkarslan@gmail.com>
2023-02-19 19:41:14 +0100

    Add example service

---
+++ golang/manual/go.mod
@@ -0,0 +1,16 @@
+module github.com/utr1903/opentelemetry-playground/golang/manual
+
+go 1.18
+
+require (
+	go.opentelemetry.io/otel v1.13.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.13.0
+	go.opentelemetry.io/otel/sdk v1.13.0
+	go.opentelemetry.io/otel/trace v1.13.0
+)
+
+require (
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
+)
---
+++ golang/manual/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"go.opentelemetry.io/otel/trace"
+)
+
+var tracer trace.Tracer
+
+func main() {
+	// Get context
+	ctx := context.Background()
+
+	// Create a new tracer provider with a batch span processor and the given exporter
+	tp := newTraceProvider()
+
+	// Handle shutdown properly so nothing leaks
+	// Cleanly shutdown and flush telemetry when the application exits.
+	defer func(ctx context.Context) {
+		// Do not make the application hang when it is shutdown.
+		ctx, cancel := context.WithTimeout(ctx, time.Second*5)
+		defer cancel()
+		if err := tp.Shutdown(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}(ctx)
+
+	// Set trace provider
+	otel.SetTracerProvider(tp)
+
+	// Set the tracer that can be used for this package
+	tracer = tp.Tracer("ExampleService")
+
+	http.HandleFunc("/", httpHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func newTraceProvider() *sdktrace.TracerProvider {
+
+	// Create exporter
+	exp, err := stdouttrace.New(
+		// Use human readable output.
+		stdouttrace.WithPrettyPrint(),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Ensure default SDK resources and the required service name are set
+	r, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("ExampleService"),
+		),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exp),
+		sdktrace.WithResource(r),
+	)
+}
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	_, span := tracer.Start(r.Context(), "hello-span")
+	defer span.End()
+
+	span.SetAttributes(
+		attribute.Bool("exampleBool", true),
+		attribute.String("exampleString", "Hey!"),
+	)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello!"))
+}
