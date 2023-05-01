32c5c410480a07ac258eb8cef4eabbe305459ca0
uturkarslan <ugurturkarslan@gmail.com>
2023-05-01 19:27:29 +0200

    Add HTTP client duration metric

--- java/apps/simulator/src/main/java/com/newrelic/otelplayground/simulator/services/HttpClient.java
+++ java/apps/simulator/src/main/java/com/newrelic/otelplayground/simulator/services/HttpClient.java
@@ -12,11 +12,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.metrics.LongHistogram;
+import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.trace.Tracer;
@@ -34,6 +38,7 @@ public class HttpClient implements CommandLineRunner {
 
   private Tracer tracer;
   private TextMapPropagator propagator;
+  private final LongHistogram httpClientDuration;
 
   @Value(value = "${HTTP_SERVER_ENDPOINT}")
   private String httpserverEndpoint;
@@ -53,6 +58,14 @@ public class HttpClient implements CommandLineRunner {
 
     // Initialize propagator
     propagator = openTelemetry.getPropagators().getTextMapPropagator();
+
+    // Initialize meter
+    Meter meter = openTelemetry.getMeter(HttpClient.class.getName());
+    httpClientDuration = meter
+        .histogramBuilder("http.client.duration")
+        .ofLongs()
+        .setDescription("Measures the duration of outbound HTTP requests.")
+        .build();
   }
 
   public void run(String... args) {
@@ -73,18 +86,17 @@ public class HttpClient implements CommandLineRunner {
 
   private void list() {
 
-    Span span = tracer.spanBuilder("GET").setSpanKind(SpanKind.CLIENT).startSpan();
+    long startTime = System.currentTimeMillis();
+    int resultStatusCode = HttpStatus.OK.value();
+
+    var url = "http://" + httpserverEndpoint + ":" + httpserverPort + "/list";
+
+    Span span = tracer.spanBuilder(HttpMethod.GET.toString()).setSpanKind(SpanKind.CLIENT).startSpan();
 
     // Make the span the current span
     try (Scope scope = span.makeCurrent()) {
-      var url = "http://" + httpserverEndpoint + ":" + httpserverPort + "/list";
 
-      span.setAttribute(SemanticAttributes.HTTP_METHOD, HttpMethod.GET.toString());
-      span.setAttribute(SemanticAttributes.HTTP_FLAVOR, HttpFlavorValues.HTTP_1_1.toString());
-      span.setAttribute(SemanticAttributes.HTTP_SCHEME, "http");
-      span.setAttribute(SemanticAttributes.HTTP_URL, url);
-      span.setAttribute(SemanticAttributes.NET_PEER_NAME, httpserverEndpoint);
-      span.setAttribute(SemanticAttributes.NET_PEER_PORT, Integer.parseInt(httpserverPort));
+      setCommonSpanAttributes(span, HttpMethod.GET, url);
 
       var headers = new HttpHeaders();
       headers.setContentType(MediaType.APPLICATION_JSON);
@@ -95,33 +107,33 @@ public class HttpClient implements CommandLineRunner {
       var entity = new HttpEntity<>(null, headers);
       var response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
 
-      span.setAttribute(SemanticAttributes.HTTP_STATUS_CODE, response.getStatusCode().value());
+      resultStatusCode = response.getStatusCode().value();
+      span.setAttribute(SemanticAttributes.HTTP_STATUS_CODE, resultStatusCode);
 
       logger.info(url + response);
     } catch (Exception e) {
-      span.setAttribute(SemanticAttributes.OTEL_STATUS_CODE, OtelStatusCodeValues.ERROR);
-      span.setAttribute(SemanticAttributes.EXCEPTION_MESSAGE, e.getMessage());
-      span.setAttribute(SemanticAttributes.EXCEPTION_STACKTRACE, e.getStackTrace().toString());
+      setExceptionSpanAttributes(span, e);
+      resultStatusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
       logger.error(e.getMessage(), e);
     } finally {
+      recordHttpClientDuration(startTime, HttpMethod.GET, url, resultStatusCode);
       span.end();
     }
   }
 
   private void delete() {
 
-    Span span = tracer.spanBuilder("DELETE").setSpanKind(SpanKind.CLIENT).startSpan();
+    long startTime = System.currentTimeMillis();
+    int resultStatusCode = HttpStatus.OK.value();
+
+    var url = "http://" + httpserverEndpoint + ":" + httpserverPort + "/delete";
+
+    Span span = tracer.spanBuilder(HttpMethod.DELETE.toString()).setSpanKind(SpanKind.CLIENT).startSpan();
 
     // Make the span the current span
     try (Scope scope = span.makeCurrent()) {
-      var url = "http://" + httpserverEndpoint + ":" + httpserverPort + "/delete";
 
-      span.setAttribute(SemanticAttributes.HTTP_METHOD, HttpMethod.DELETE.toString());
-      span.setAttribute(SemanticAttributes.HTTP_FLAVOR, HttpFlavorValues.HTTP_1_1.toString());
-      span.setAttribute(SemanticAttributes.HTTP_SCHEME, "http");
-      span.setAttribute(SemanticAttributes.HTTP_URL, url);
-      span.setAttribute(SemanticAttributes.NET_PEER_NAME, httpserverEndpoint);
-      span.setAttribute(SemanticAttributes.NET_PEER_PORT, Integer.parseInt(httpserverPort));
+      setCommonSpanAttributes(span, HttpMethod.DELETE, url);
 
       var headers = new HttpHeaders();
       headers.setContentType(MediaType.APPLICATION_JSON);
@@ -132,16 +144,46 @@ public class HttpClient implements CommandLineRunner {
       var entity = new HttpEntity<>(null, headers);
       var response = restTemplate.exchange(url, HttpMethod.DELETE, entity, String.class);
 
-      span.setAttribute(SemanticAttributes.HTTP_STATUS_CODE, response.getStatusCode().value());
+      resultStatusCode = response.getStatusCode().value();
+      span.setAttribute(SemanticAttributes.HTTP_STATUS_CODE, resultStatusCode);
 
       logger.info(url + response);
     } catch (Exception e) {
-      span.setAttribute(SemanticAttributes.OTEL_STATUS_CODE, OtelStatusCodeValues.ERROR);
-      span.setAttribute(SemanticAttributes.EXCEPTION_MESSAGE, e.getMessage());
-      span.setAttribute(SemanticAttributes.EXCEPTION_STACKTRACE, e.getStackTrace().toString());
+      setExceptionSpanAttributes(span, e);
+      resultStatusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
       logger.error(e.getMessage(), e);
     } finally {
       span.end();
+      recordHttpClientDuration(startTime, HttpMethod.DELETE, url, resultStatusCode);
     }
+  }
+
+  private void setCommonSpanAttributes(Span span, HttpMethod httpMethod, String url) {
+    span.setAttribute(SemanticAttributes.HTTP_METHOD, httpMethod.toString());
+    span.setAttribute(SemanticAttributes.HTTP_FLAVOR, HttpFlavorValues.HTTP_1_1.toString());
+    span.setAttribute(SemanticAttributes.HTTP_SCHEME, "http");
+    span.setAttribute(SemanticAttributes.HTTP_URL, url);
+    span.setAttribute(SemanticAttributes.NET_PEER_NAME, httpserverEndpoint);
+    span.setAttribute(SemanticAttributes.NET_PEER_PORT, Integer.parseInt(httpserverPort));
+  }
+
+  private void setExceptionSpanAttributes(Span span, Exception e) {
+    span.setAttribute(SemanticAttributes.OTEL_STATUS_CODE, OtelStatusCodeValues.ERROR);
+    span.setAttribute(SemanticAttributes.EXCEPTION_MESSAGE, e.getMessage());
+    span.setAttribute(SemanticAttributes.EXCEPTION_STACKTRACE, e.getStackTrace().toString());
+  }
+
+  private void recordHttpClientDuration(long startTime, HttpMethod httpMethod, String url, int httpStatus) {
+    long duration = System.currentTimeMillis() - startTime;
+    Attributes attrs = Attributes.builder()
+        .put(SemanticAttributes.HTTP_METHOD.toString(), httpMethod.toString())
+        .put(SemanticAttributes.HTTP_FLAVOR, HttpFlavorValues.HTTP_1_1.toString())
+        .put(SemanticAttributes.HTTP_SCHEME.toString(), "http")
+        .put(SemanticAttributes.HTTP_URL.toString(), url)
+        .put(SemanticAttributes.NET_PEER_NAME.toString(), httpserverEndpoint)
+        .put(SemanticAttributes.NET_PEER_PORT.toString(), Integer.parseInt(httpserverPort))
+        .put(SemanticAttributes.HTTP_STATUS_CODE.toString(), httpStatus)
+        .build();
+    httpClientDuration.record(duration, attrs);
   }
 }
