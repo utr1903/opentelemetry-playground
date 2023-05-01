917a6cbb441d75e382b0c30e7f75230bc5f7c773
uturkarslan <ugurturkarslan@gmail.com>
2023-05-01 19:48:41 +0200

    Convert all cumulative metrics to delta

--- infra/scripts/02_deploy_apps.sh
+++ infra/scripts/02_deploy_apps.sh
@@ -108,79 +108,78 @@ if [[ $build == "true" ]]; then
     "../../${language}/apps/httpserver/."
   docker push "${DOCKERHUB_NAME}/${httpserver[imageName]}"
 
-  # # kafkaconsumer
-  # docker build \
-  #   --platform "linux/${platform}" \
-  #   --tag "${DOCKERHUB_NAME}/${kafkaconsumer[imageName]}" \
-  #   "../../${language}/apps/kafkaconsumer/."
-  # docker push "${DOCKERHUB_NAME}/${kafkaconsumer[imageName]}"
+  # kafkaconsumer
+  docker build \
+    --platform "linux/${platform}" \
+    --tag "${DOCKERHUB_NAME}/${kafkaconsumer[imageName]}" \
+    "../../${language}/apps/kafkaconsumer/."
+  docker push "${DOCKERHUB_NAME}/${kafkaconsumer[imageName]}"
 
-  # # simulator
-  # docker build \
-  #   --platform "linux/${platform}" \
-  #   --tag "${DOCKERHUB_NAME}/${simulator[imageName]}" \
-  #   "../../${language}/apps/simulator/."
-  # docker push "${DOCKERHUB_NAME}/${simulator[imageName]}"
+  # simulator
+  docker build \
+    --platform "linux/${platform}" \
+    --tag "${DOCKERHUB_NAME}/${simulator[imageName]}" \
+    "../../${language}/apps/simulator/."
+  docker push "${DOCKERHUB_NAME}/${simulator[imageName]}"
 fi
 
 ###################
 ### Deploy Helm ###
 ###################
 
-# # Add helm repos
-# helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
-# helm repo add bitnami https://charts.bitnami.com/bitnami
-# helm repo update
+# Add helm repos
+helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
 
-# # kafka
-# helm upgrade ${kafka[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace=${kafka[namespace]} \
-#   "bitnami/kafka"
+# kafka
+helm upgrade ${kafka[name]} \
+  --install \
+  --wait \
+  --debug \
+  --create-namespace \
+  --namespace=${kafka[namespace]} \
+  "bitnami/kafka"
 
-# # mysql
-# helm upgrade ${mysql[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace=${mysql[namespace]} \
-#   --set auth.rootPassword=${mysql[password]} \
-#   --set auth.database=${mysql[database]} \
-#     "bitnami/mysql"
+# mysql
+helm upgrade ${mysql[name]} \
+  --install \
+  --wait \
+  --debug \
+  --create-namespace \
+  --namespace=${mysql[namespace]} \
+  --set auth.rootPassword=${mysql[password]} \
+  --set auth.database=${mysql[database]} \
+    "bitnami/mysql"
 
-# # otelcollector
-# helm upgrade ${otelcollector[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace ${otelcollector[namespace]} \
-#   --set mode=${otelcollector[mode]} \
-#   --set config.receivers.jaeger=null \
-#   --set config.receivers.prometheus=null \
-#   --set config.receivers.zipkin=null \
-#   --set config.processors.cumulativetodelta.include.match_type="strict" \
-#   --set config.processors.cumulativetodelta.include.metrics[0]="http.server.duration" \
-#   --set config.processors.cumulativetodelta.include.metrics[1]="http.client.duration" \
-#   --set config.exporters.logging=null \
-#   --set config.exporters.otlp.endpoint="otlp.eu01.nr-data.net:4317" \
-#   --set config.exporters.otlp.tls.insecure=false \
-#   --set config.exporters.otlp.headers.api-key=$NEWRELIC_LICENSE_KEY \
-#   --set config.service.pipelines.traces.receivers[0]="otlp" \
-#   --set config.service.pipelines.traces.processors[0]="batch" \
-#   --set config.service.pipelines.traces.processors[1]="memory_limiter" \
-#   --set config.service.pipelines.traces.exporters[0]="otlp" \
-#   --set config.service.pipelines.metrics.receivers[0]="otlp" \
-#   --set config.service.pipelines.metrics.processors[0]="batch" \
-#   --set config.service.pipelines.metrics.processors[1]="memory_limiter" \
-#   --set config.service.pipelines.metrics.processors[2]="cumulativetodelta" \
-#   --set config.service.pipelines.metrics.exporters[0]="otlp" \
-#   --set config.service.pipelines.logs=null \
-#   "open-telemetry/opentelemetry-collector"
+# otelcollector
+helm upgrade ${otelcollector[name]} \
+  --install \
+  --wait \
+  --debug \
+  --create-namespace \
+  --namespace ${otelcollector[namespace]} \
+  --set mode=${otelcollector[mode]} \
+  --set config.receivers.jaeger=null \
+  --set config.receivers.prometheus=null \
+  --set config.receivers.zipkin=null \
+  --set config.processors.cumulativetodelta.include.match_type="regexp" \
+  --set config.processors.cumulativetodelta.include.metrics[0]="*" \
+  --set config.exporters.logging=null \
+  --set config.exporters.otlp.endpoint="otlp.eu01.nr-data.net:4317" \
+  --set config.exporters.otlp.tls.insecure=false \
+  --set config.exporters.otlp.headers.api-key=$NEWRELIC_LICENSE_KEY \
+  --set config.service.pipelines.traces.receivers[0]="otlp" \
+  --set config.service.pipelines.traces.processors[0]="batch" \
+  --set config.service.pipelines.traces.processors[1]="memory_limiter" \
+  --set config.service.pipelines.traces.exporters[0]="otlp" \
+  --set config.service.pipelines.metrics.receivers[0]="otlp" \
+  --set config.service.pipelines.metrics.processors[0]="batch" \
+  --set config.service.pipelines.metrics.processors[1]="memory_limiter" \
+  --set config.service.pipelines.metrics.processors[2]="cumulativetodelta" \
+  --set config.service.pipelines.metrics.exporters[0]="otlp" \
+  --set config.service.pipelines.logs=null \
+  "open-telemetry/opentelemetry-collector"
 
 # httpserver
 helm upgrade ${httpserver[name]} \
@@ -205,50 +204,50 @@ helm upgrade ${httpserver[name]} \
   --set otlp.endpoint="http://${otelcollector[name]}-opentelemetry-collector.${otelcollector[namespace]}.svc.cluster.local:4317" \
   "../helm/httpserver"
 
-# # kafkaconsumer
-# helm upgrade ${kafkaconsumer[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace=${kafkaconsumer[namespace]} \
-#   --set dockerhubName=$DOCKERHUB_NAME \
-#   --set imageName=${kafkaconsumer[imageName]} \
-#   --set imagePullPolicy="Always" \
-#   --set name=${kafkaconsumer[name]} \
-#   --set replicas=${kafkaconsumer[replicas]} \
-#   --set kafka.address="${kafka[name]}.${kafka[namespace]}.svc.cluster.local:9092" \
-#   --set kafka.topic=${kafka[topic]} \
-#   --set kafka.groupId=${kafkaconsumer[name]} \
-#   --set mysql.server="${mysql[name]}.${mysql[namespace]}.svc.cluster.local" \
-#   --set mysql.username=${mysql[username]} \
-#   --set mysql.password=${mysql[password]} \
-#   --set mysql.port=${mysql[port]} \
-#   --set mysql.database=${mysql[database]} \
-#   --set mysql.table=${mysql[table]} \
-#   --set otel.exporter="otlp" \
-#   --set otlp.endpoint="http://${otelcollector[name]}-opentelemetry-collector.${otelcollector[namespace]}.svc.cluster.local:4317" \
-#   "../helm/kafkaconsumer"
+# kafkaconsumer
+helm upgrade ${kafkaconsumer[name]} \
+  --install \
+  --wait \
+  --debug \
+  --create-namespace \
+  --namespace=${kafkaconsumer[namespace]} \
+  --set dockerhubName=$DOCKERHUB_NAME \
+  --set imageName=${kafkaconsumer[imageName]} \
+  --set imagePullPolicy="Always" \
+  --set name=${kafkaconsumer[name]} \
+  --set replicas=${kafkaconsumer[replicas]} \
+  --set kafka.address="${kafka[name]}.${kafka[namespace]}.svc.cluster.local:9092" \
+  --set kafka.topic=${kafka[topic]} \
+  --set kafka.groupId=${kafkaconsumer[name]} \
+  --set mysql.server="${mysql[name]}.${mysql[namespace]}.svc.cluster.local" \
+  --set mysql.username=${mysql[username]} \
+  --set mysql.password=${mysql[password]} \
+  --set mysql.port=${mysql[port]} \
+  --set mysql.database=${mysql[database]} \
+  --set mysql.table=${mysql[table]} \
+  --set otel.exporter="otlp" \
+  --set otlp.endpoint="http://${otelcollector[name]}-opentelemetry-collector.${otelcollector[namespace]}.svc.cluster.local:4317" \
+  "../helm/kafkaconsumer"
 
-# # simulator
-# helm upgrade ${simulator[name]} \
-#   --install \
-#   --wait \
-#   --debug \
-#   --create-namespace \
-#   --namespace=${simulator[namespace]} \
-#   --set dockerhubName=$DOCKERHUB_NAME \
-#   --set imageName=${simulator[imageName]} \
-#   --set imagePullPolicy="Always" \
-#   --set name=${simulator[name]} \
-#   --set replicas=${simulator[replicas]} \
-#   --set port=${simulator[port]} \
-#   --set httpserver.requestInterval=${simulator[httpInterval]} \
-#   --set httpserver.endpoint="${httpserver[name]}.${httpserver[namespace]}.svc.cluster.local" \
-#   --set httpserver.port="${httpserver[port]}" \
-#   --set kafka.address="${kafka[name]}-0.${kafka[name]}-headless.${kafka[namespace]}.svc.cluster.local:9092" \
-#   --set kafka.topic=${kafka[topic]} \
-#   --set kafka.requestInterval=${simulator[kafkaInterval]} \
-#   --set otel.exporter="otlp" \
-#   --set otlp.endpoint="http://${otelcollector[name]}-opentelemetry-collector.${otelcollector[namespace]}.svc.cluster.local:4317" \
-#   "../helm/simulator"
+# simulator
+helm upgrade ${simulator[name]} \
+  --install \
+  --wait \
+  --debug \
+  --create-namespace \
+  --namespace=${simulator[namespace]} \
+  --set dockerhubName=$DOCKERHUB_NAME \
+  --set imageName=${simulator[imageName]} \
+  --set imagePullPolicy="Always" \
+  --set name=${simulator[name]} \
+  --set replicas=${simulator[replicas]} \
+  --set port=${simulator[port]} \
+  --set httpserver.requestInterval=${simulator[httpInterval]} \
+  --set httpserver.endpoint="${httpserver[name]}.${httpserver[namespace]}.svc.cluster.local" \
+  --set httpserver.port="${httpserver[port]}" \
+  --set kafka.address="${kafka[name]}-0.${kafka[name]}-headless.${kafka[namespace]}.svc.cluster.local:9092" \
+  --set kafka.topic=${kafka[topic]} \
+  --set kafka.requestInterval=${simulator[kafkaInterval]} \
+  --set otel.exporter="otlp" \
+  --set otlp.endpoint="http://${otelcollector[name]}-opentelemetry-collector.${otelcollector[namespace]}.svc.cluster.local:4317" \
+  "../helm/simulator"
