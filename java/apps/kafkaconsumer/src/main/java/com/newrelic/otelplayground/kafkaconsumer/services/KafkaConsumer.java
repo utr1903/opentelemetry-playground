306f3d4764f264c7ba75ec1f3477891f13355479
uturkarslan <ugurturkarslan@gmail.com>
2023-06-19 20:54:07 +0200

    Add database span

--- java/apps/kafkaconsumer/src/main/java/com/newrelic/otelplayground/kafkaconsumer/services/KafkaConsumer.java
+++ java/apps/kafkaconsumer/src/main/java/com/newrelic/otelplayground/kafkaconsumer/services/KafkaConsumer.java
@@ -1,13 +1,17 @@
 package com.newrelic.otelplayground.kafkaconsumer.services;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.Headers;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +25,11 @@ import com.newrelic.otelplayground.kafkaconsumer.repositories.NameRepository;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.TextMapGetter;
+import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.instrumentation.kafkaclients.v2_6.KafkaTelemetry;
 import io.opentelemetry.instrumentation.kafkaclients.v2_6.TracingConsumerInterceptor;
 import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
@@ -32,6 +41,28 @@ public class KafkaConsumer implements CommandLineRunner {
   private final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
 
   private OpenTelemetry openTelemetry;
+  private TextMapGetter<Headers> textMapGetter = new TextMapGetter<Headers>() {
+
+    @Override
+    public Iterable<String> keys(Headers headers) {
+      List<String> keys = new ArrayList<>();
+      for (Header header : headers)
+        keys.add(header.key());
+      // System.out.println(keys);
+      return keys;
+    }
+
+    @Override
+    public String get(Headers headers, String key) {
+      System.out.println(key);
+      Header header = headers.lastHeader(key);
+      if (header != null) {
+        // System.out.println(new String(header.value()));
+        return new String(header.value());
+      }
+      return null;
+    }
+  };
 
   private Consumer<String, String> consumer;
 
@@ -113,12 +144,59 @@ public class KafkaConsumer implements CommandLineRunner {
     while (true) {
       ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
       for (ConsumerRecord<String, String> record : records) {
-        logger.info("Storing name...");
-        Name name = new Name();
-        name.setName(record.value());
-        repository.save(name);
-        logger.info("Name is stored.");
+
+        // Extract context
+        TextMapPropagator propagator = openTelemetry.getPropagators().getTextMapPropagator();
+        Context ctx = propagator.extract(Context.current(), record.headers(),
+            textMapGetter);
+
+        String dbStatement = DB_OPERATION + " INTO name " + mysqlTable + " FROM " + mysqlTable;
+
+        // Create span from current context
+        // Span span = Span.fromContext(ctx);
+        Span dbSpan = openTelemetry.getTracer(KafkaConsumer.class.getName())
+            .spanBuilder(dbStatement)
+            .setParent(ctx)
+            .setSpanKind(SpanKind.CLIENT)
+            .startSpan();
+        // ctx = ctx.with(dbSpan);
+
+        // logger.info(dbSpan.getSpanContext().getTraceId());
+
+        try (Scope scope = dbSpan.makeCurrent()) {
+          setCommonSpanAttributes(dbSpan, dbStatement);
+
+          logger.info("Storing name...");
+          Name name = new Name();
+          name.setName(record.value());
+          repository.save(name);
+          logger.info("Name is stored.");
+
+        } catch (Exception e) {
+          setExceptionSpanAttributes(dbSpan, e);
+          logger.error(e.getMessage(), e);
+        } finally {
+          dbSpan.end();
+        }
       }
     }
+  }
+
+  private void setCommonSpanAttributes(Span span, String dbStatement) {
+    span.setAttribute(SemanticAttributes.DB_SYSTEM, "mysql");
+    span.setAttribute(SemanticAttributes.DB_USER, mysqlUser);
+    span.setAttribute(SemanticAttributes.DB_NAME, mysqlDatabase);
+    span.setAttribute(SemanticAttributes.DB_SQL_TABLE, mysqlTable);
+    span.setAttribute(SemanticAttributes.NET_PEER_NAME, mysqlServer);
+    span.setAttribute(SemanticAttributes.NET_PEER_PORT, Integer.parseInt(mysqlPort));
+    span.setAttribute(SemanticAttributes.NET_TRANSPORT, "IP.TCP");
+    span.setAttribute(SemanticAttributes.DB_OPERATION, DB_OPERATION);
+    span.setAttribute(SemanticAttributes.DB_STATEMENT, dbStatement);
+  }
+
+  private void setExceptionSpanAttributes(Span span, Exception e) {
+    span.setAttribute(SemanticAttributes.OTEL_STATUS_CODE, OtelStatusCodeValues.ERROR);
+    span.setAttribute(SemanticAttributes.EXCEPTION_MESSAGE, e.getMessage());
+    span.setAttribute(SemanticAttributes.EXCEPTION_STACKTRACE, e.getStackTrace().toString());
   }
 }
