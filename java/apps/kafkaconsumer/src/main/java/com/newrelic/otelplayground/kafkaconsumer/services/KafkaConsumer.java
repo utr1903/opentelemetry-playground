755d2124237a820522a9033f64e06b52b14339ad
uturkarslan <ugurturkarslan@gmail.com>
2023-06-19 17:21:08 +0200

    Implement database save

--- java/apps/kafkaconsumer/src/main/java/com/newrelic/otelplayground/kafkaconsumer/services/KafkaConsumer.java
+++ java/apps/kafkaconsumer/src/main/java/com/newrelic/otelplayground/kafkaconsumer/services/KafkaConsumer.java
@@ -11,13 +11,20 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.newrelic.otelplayground.kafkaconsumer.entities.Name;
+import com.newrelic.otelplayground.kafkaconsumer.repositories.NameRepository;
+
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.instrumentation.kafkaclients.v2_6.KafkaTelemetry;
 import io.opentelemetry.instrumentation.kafkaclients.v2_6.TracingConsumerInterceptor;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes.OtelStatusCodeValues;
 
 @Component
 public class KafkaConsumer implements CommandLineRunner {
@@ -36,6 +43,26 @@ public class KafkaConsumer implements CommandLineRunner {
 
   @Value(value = "${KAFKA_CONSUMER_GROUP_ID}")
   private String kafkaConsumerGroupId;
+
+  @Value(value = "${MYSQL_SERVER}")
+  private String mysqlServer;
+
+  @Value(value = "${MYSQL_USERNAME}")
+  private String mysqlUser;
+
+  @Value(value = "${MYSQL_PORT}")
+  private String mysqlPort;
+
+  @Value(value = "${MYSQL_DATABASE}")
+  private String mysqlDatabase;
+
+  @Value(value = "${MYSQL_TABLE}")
+  private String mysqlTable;
+
+  private final String DB_OPERATION = "INSERT";
+
+  @Autowired
+  private NameRepository repository;
 
   public KafkaConsumer(OpenTelemetry openTelemetry) {
     this.openTelemetry = openTelemetry;
@@ -86,10 +113,11 @@ public class KafkaConsumer implements CommandLineRunner {
     while (true) {
       ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
       for (ConsumerRecord<String, String> record : records) {
-        logger.info(record.topic());
-        logger.info(record.value());
-        logger.info(String.valueOf(record.offset()));
-        logger.info(String.valueOf(record.partition()));
+        logger.info("Storing name...");
+        Name name = new Name();
+        name.setName(record.value());
+        repository.save(name);
+        logger.info("Name is stored.");
       }
     }
   }
