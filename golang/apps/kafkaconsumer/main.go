8dc10833f865ad831c3358571aa317ae9c4b1d19
uturkarslan <ugurturkarslan@gmail.com>
2023-12-25 17:50:35 +0100

    Refactor MySQL

--- golang/apps/kafkaconsumer/main.go
+++ golang/apps/kafkaconsumer/main.go
@@ -37,9 +37,17 @@ func main() {
 		panic(err)
 	}
 
-	// Connect to MySQL
-	mysql.CreateDatabaseConnection(cfg)
-	defer mysql.Get().Close()
+	// Instantiate MySQL database
+	db := mysql.New(
+		mysql.WithServer(cfg.MysqlServer),
+		mysql.WithPort(cfg.MysqlPort),
+		mysql.WithUsername(cfg.MysqlUsername),
+		mysql.WithPassword(cfg.MysqlPassword),
+		mysql.WithDatabase(cfg.MysqlDatabase),
+		mysql.WithTable(cfg.MysqlTable),
+	)
+	db.CreateDatabaseConnection()
+	defer db.Instance.Close()
 
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
 	defer cancel()
